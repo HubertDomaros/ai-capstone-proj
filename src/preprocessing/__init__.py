9bcb26aa16d0e58a1f252ab4ff43d367242c3921
HubertDomaros <38128396+HubertDomaros@users.noreply.github.com>
2025-01-01 20:09:58 +0100

    bugfixes

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,2 +1,2 @@
 from .AnnotationsParser import AnnotationsParser
-from .image_split import train_test_val_image_split, dict_to_json
+from .image_splitter import train_test_val_image_split, dict_to_json
