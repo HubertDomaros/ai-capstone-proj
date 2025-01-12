1a5e40ebb0c5813b1b3aae198abadfedd0250b48
HubertDomaros <38128396+HubertDomaros@users.noreply.github.com>
2025-01-12 01:13:23 +0100

    added handling of missing images, since xml annotations doesn't exist for all images + saving bbox labels in yolo txt files

--- src/annotations_processing.py
+++ src/annotations_processing.py
@@ -3,6 +3,7 @@ import re
 
 import xmltodict
 import pandas as pd
+import imagesize
 
 from . import constants as c
 from . import utils as u
@@ -78,15 +79,59 @@ def xml_annotations_to_dataframe(folder_path: str) -> pd.DataFrame:
     lst = u.unpack_lists(list_of_dicts=list_of_dicts, col_list=c.columns_list)
     return pd.DataFrame(lst)
 
-# def fill_missing_imgs_as_backgrounds(df: pd.DataFrame) -> pd.DataFrame:
-#     img_list
-#
-#     first = int(re.search('\d+', img_list[0]).group(0))
-#     last = int(re.search('\d+', img_list[-1]).group(0))
 
+def fill_missing_imgs_in_df(img_folder_path, input_df):
+    """
+    Adds missing images from a folder to a DataFrame containing image metadata.
+    For each image in the folder that's not in the input DataFrame, gets its dimensions
+    and creates a new row with default values. Returns concatenated DataFrame with new entries.
 
+    Args:
+        img_folder_path (str): Path to folder containing images
+        input_df (pd.DataFrame): DataFrame with existing image metadata
 
+    Returns:
+        pd.DataFrame: Original DataFrame with new rows for missing images,
+        or original DataFrame if no new images found
+    """
+    # Get list of images
+    img_list = []
+    for dirpath, dirnames, filenames in os.walk(img_folder_path):
+        if dirpath == img_folder_path:
+            img_list = filenames
+            break
 
+    # Process images sequentially
+    results = []
+    for img in img_list:
+        if img not in input_df['img'].tolist():
+            img_path = os.path.join(os.getcwd(), img_folder_path, img)
+            # using awesome imagesize lib! Super fast! super cool!
+            # https://github.com/shibukawa/imagesize_py
+            shape = imagesize.get(img_path)
+            l = [0] * len(c.columns_list)
+            l[0] = img
+            l[1] = shape[0]
+            l[2] = shape[1]
+            l[7] = 1
+            results.append(l)
+
+    # Filter out None results and add to DataFrame
+    out_df = 0
+    out_df = pd.concat([input_df, pd.DataFrame(results, columns=c.columns_list)], ignore_index=True)
+    return out_df
+
+def save_yolo_annotations(df: pd.DataFrame, out_folder: str):
+    i = 0
+    for img, data in df.groupby('img'):
+        img = str(img)
+        filename = f'{img.split('.')[0]}.txt'
+        output = data[['multi_hot_encoding_class',
+                                'xcenter', 'ycenter', 'width', 'height']]
+        output.to_csv(os.path.join(out_folder, filename), index=False, header=False, sep = " ")
+        i += 1
+
+    print(f'Saved {i} annotations to {out_folder}')
 
 
 if __name__ == '__main__':
